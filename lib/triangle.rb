56124e11564a876b2a4d659a8ae73ff90a5a5a5b
An Pham <anpham148@gmail.com>
2020-07-21 13:47:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
        :isosceles
      elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
        :scalene
-     elsif @side1 =< 0 && side2 =< 0 && side3 =< 0
+     elsif @side1 <= 0 && side2 <= 0 && side3 <= 0
        begin
           raise TriangleError
        rescue TriangleError => error
